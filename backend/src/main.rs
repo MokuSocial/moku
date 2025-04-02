b935e7f833636d11da9173f02e897c6fed9edfe2
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2025-04-02 12:57:12 +0200

    feat(backend): Axum working
    Beginning of dev branch

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    // Configura il router con una route di test
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Axum!" }))
+        .layer(CorsLayer::new().allow_origin(Any));
+
+    // Imposta l'indirizzo del server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server in esecuzione su http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    // Avvia il server
+    axum::serve(listener, app.into_make_service()).await.unwrap();
 }
+
