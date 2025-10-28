d820d9fe0b6bd02618cf5acc4caeedae377d78f0
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2025-10-28 12:08:47 +0100

    feat: update init.sql

--- res/init.sql
+++ res/init.sql
@@ -1,28 +1,10 @@
-CREATE TABLE IF NOT EXISTS users (
-	username TEXT PRIMARY KEY NOT NULL,  -- Identificatore dell'utente (stringa)
-	password_hash TEXT NOT NULL,
-	email TEXT UNIQUE NOT NULL,
-	created_at INTEGER NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS ingredients (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL UNIQUE,
-	identifier TEXT UNIQUE NOT NULL, 
-	wikidata TEXT,
-	cost_per_unit REAL,
+	-- identifier TEXT UNIQUE NOT NULL, 
+	-- wikidata TEXT,
+	-- cost_per_unit REAL,
 	unit TEXT CHECK (unit IN ('kg', 'g', 'l', 'ml', 'piece'))  -- Unità di misura
-);
-
-CREATE TABLE IF NOT EXISTS recipes (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	user_id TEXT NOT NULL,  -- Identificativo utente come stringa
-	title TEXT NOT NULL,
-	introduction TEXT NOT NULL,
-	conclusion TEXT NOT NULL,
-	created_at INTEGER NOT NULL,
-	last_updated INTEGER DEFAULT NULL,
-	FOREIGN KEY(user_id) REFERENCES users(username) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS recipe_ingredients (
@@ -34,6 +16,18 @@ CREATE TABLE IF NOT EXISTS recipe_ingredients (
 	FOREIGN KEY(ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS recipes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	author TEXT NOT NULL,  -- Identificativo utente come stringa
+	title TEXT NOT NULL,
+	banner_image_url TEXT,
+	-- introduction TEXT NOT NULL,
+	-- conclusion TEXT NOT NULL,
+	created_at INTEGER NOT NULL,
+	last_updated INTEGER DEFAULT NULL,
+	FOREIGN KEY(author) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS recipe_steps (
 	recipe_id INTEGER NOT NULL,
 	step_number INTEGER NOT NULL,
@@ -43,14 +37,29 @@ CREATE TABLE IF NOT EXISTS recipe_steps (
 	FOREIGN KEY(recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS tags (
+CREATE TABLE IF NOT EXISTS users (
+	username TEXT PRIMARY KEY NOT NULL,  -- Identificatore dell'utente (stringa)
+	password_hash TEXT NOT NULL,
+	email TEXT UNIQUE NOT NULL,
+	created_at INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tag (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL UNIQUE
+	text TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS recipe_tags (
 	recipe_id INTEGER NOT NULL,
 	tag_id INTEGER NOT NULL,
+	FOREIGN KEY(recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+	FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS indications (
+	recipe_id INTEGER NOT NULL,
+	tag_id INTEGER NOT NULL,
+	value TEXT NOT NULL,
 	FOREIGN KEY(recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
 	FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
